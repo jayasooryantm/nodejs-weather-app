8da344a4ba0f6442eb00b15b7d88a1af3299af75
Jayasooryan <jayasooryantm2@gmail.com>
2020-07-25 07:46:44 +0530

    deployment changes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const geocode = require('./utils/geo-code')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -83,6 +85,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Weather API Running')
 })
